3a4700d02fd6491b1eea8948f0f6fc10e101f32c
Vedant005 <vedantkanekar909@gmail.com>
2025-07-08 20:47:30 +0530

    feat: create Task model with schema and default values

---
+++ backend/src/models/task.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true }, // Uniqueness validation for board later
+    description: { type: String },
+    assignedUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      default: null,
+    },
+    status: {
+      type: String,
+      enum: ["Todo", "In Progress", "Done"],
+      default: "Todo",
+    },
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    version: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+export const Task = mongoose.model("Task", TaskSchema);
