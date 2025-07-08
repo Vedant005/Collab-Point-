337765183e861b39cba3f9db5651233c6197b14a
Vedant005 <vedantkanekar909@gmail.com>
2025-07-08 20:47:20 +0530

    feat: implement user authentication routes and controller methods

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+  getCurrentUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.route("/login").post(loginUser);
+
+router.route("/register").post(registerUser);
+
+router.route("/logout").post(logoutUser);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+export default router;
